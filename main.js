e42ead666eae2edc099e7609e0d23e0be8f47289
Costahadassa <costahdassa@gmail.com>
2020-11-20 16:58:41 -0300

    adicionando funcionalidade ao botao

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/rickandmorty/rickandmorty.js';
+
+
+function validar (evento) {
+  const texto = document.getElementById("txtBusca").value;
+
+  console.log(texto)
+}
+
+const btnBusca = document.getElementById("buscar");
+btnBusca.addEventListener('click',validar)
+
 
 console.log(example, data);
