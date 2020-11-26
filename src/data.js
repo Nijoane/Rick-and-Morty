b13419779eafd6694db1a6b9b688070e69bc5a33
Costahadassa <costahadassa@gmail.com>
2020-11-26 18:16:21 -0300

    iniciando funcionalidade de filter

--- src/data.js
+++ src/data.js
@@ -1,9 +1,19 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+function getEstatus(getPersonagens, estatus) {
+  let resultFilter = getPersonagens.filter (el => el.estatus.includes(estatus));
+  return resultFilter;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+function percentEstatus(estatus, resultFilter) {
+  const perc = (resultFilter.length *100)/estatus.length;
+  return Math.round(perc);
+}
+
+function getSpecies(getPersonagens, especies) {
+  let resultSpecies = getPersonagens.filter(el => el.especies.includes(Species));
+  return resultEspecies;
 };
+
+function percentEspecies(especies, resultSpecies) {
+  let percent = (resultEspecies.length *100)/especies.length;
+  return Math.round(percent);
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/rickandmorty/rickandmorty.js';
+
+
+function validar (evento) {
+  const texto = document.getElementById("txtBusca").value;
+
+  console.log(texto)
+}
+
+const btnBusca = document.getElementById("buscar");
+btnBusca.addEventListener('click',validar)
+
 
 console.log(example, data);
+
+const statusMenu = document.getElementById("filtro-status");
+const speciesMenu = document.getElementById("filtro-species");
+
+
+reload.addEventListener("click", refreshPage);
+
+statusMenu.addEventListener("change", () => {
+  const retorno = window.data.getStatus(getCharacters, statusMenu.value);
+  show(retorno);
+  calc.innerHTML= percentStatus(getCharacters, retorno) + "%";
+});
