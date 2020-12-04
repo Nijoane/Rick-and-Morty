b40653a7e05da533236dc2fae8f3c4c198395e97
Nijoane <nicajoane@outlook.com>
2020-12-04 19:48:52 -0300

    alterando fetch

--- src/data.js
+++ src/data.js
@@ -2,25 +2,8 @@ export const searchCharacter = (data, name) => {
   return data.filter(results => results.name.includes(name));
 }
 
-export const filterSort = (data, order) => {
-  if(order == "a-z"){
-    return data.sort(function(a, b){
-      if(a.name >= b.name){
-        return -1;
+export const orderAZ =(data) => data.sort((a, b) => (a.name < b.name) ? - 1 : 1);
 
-      }else {
-        return 1;
-      }
-    });
-    
-  } else {
-    return data.sort(function(a, b){
-      if(a.name <= b.name){
-        return 1;
+export const orderZA =(data) => data.sort((a, b) => (a.name > b.name) ? - 1 : 1);
 
-      } else{
-        return -1;
-      }
-    });  
-  }
-}
+
