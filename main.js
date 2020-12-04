dae9c991f606914d0e723b41116e574e66c9fbba
Nijoane <nicajoane@outlook.com>
2020-12-03 21:54:28 -0300

    adiconando funções filtre ao DOM

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-// import { filterSort } from './data.js';
+/* eslint-disable no-console */
+import { filterSort, searchCharacter } from './data.js';
+import data from './data/rickandmorty/rickandmorty.js';
+
 
 const fetchCharacter = () => {
   const characterId = 400;
@@ -9,7 +12,6 @@ const fetchCharacter = () => {
 
     promise.push(fetch(baseUrl).then((response) => response.json()));
   }   
-  
   // eslint-disable-next-line no-undef
   Promise.all(promise).then((resolve) => {
     const character = resolve.map((output) => ({
@@ -21,6 +23,7 @@ const fetchCharacter = () => {
       origin: output.origin.name,
       location: output.location.name,
     }));
+  
     createCard(character);
   });
 };
@@ -28,36 +31,43 @@ fetchCharacter();
 
 const cardElement = document.getElementById("container-card")
 const createCard = (character) => {
-  const createCharacterCard = character.map((characters) => `
-    <li id="create-card">
-      <div class="info-card">
-        <span class="character-name" id="name">${characters.name}</span>
-        <img 
-          class="character-image"
-          src="${characters.image}" 
-          alt="character-image"
-        >
-      </div>
-      <aside class="card-modal">
-        <span class="span-modal">Status: ${characters.status}</span>
-        <span class="span-modal">Specie: ${characters.species}</span>
-        <span class="span-modal">Origin: ${characters.origin}</span>
-        <span class="span-modal">Location: ${characters.location}</span>
-      </aside>
-    </li>
-  ` 
-  )
+  const createCharacterCard = character.map((characters) => {
+    return `
+      <li id="create-card">
+        <div class="info-card">
+          <span class="character-name" id="name">${characters.name}</span>
+          <img 
+            class="character-image"
+            src="${characters.image}" 
+            alt="character-image"
+          >
+        </div>
+        <aside class="card-modal">
+          <span class="span-modal">Status: ${characters.status}</span>
+          <span class="span-modal">Specie: ${characters.species}</span>
+          <span class="span-modal">Origin: ${characters.origin}</span>
+          <span class="span-modal">Location: ${characters.location}</span>
+        </aside>
+      </li>
+    `
+  })
   .join('')
   cardElement.innerHTML = createCharacterCard;
 };
 
+document.getElementById("filters").addEventListener("change", () => {
+  const listSort = document.getElementById("filters").value;
+  const filterOrder = (filterSort(data.results, listSort));
+
+  createCard(filterOrder);
+});
 
 
 const btnBusca = document.getElementById("buscar");
-function validar (event,){
-  event.preventDefault();
-  const texto = document.getElementById("txtBusca").value;
-  // eslint-disable-next-line no-console
-  console.log(texto);
-}
-btnBusca.addEventListener('keyup', validar);
+const txtSearch = document.getElementById("txtBusca")
+btnBusca.addEventListener('click', () => {
+  const characterName = txtSearch.value;
+  const results = searchCharacter(data.results, characterName);
+  
+  createCard(results);
+});
