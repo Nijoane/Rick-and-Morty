551aeb80e50334e8d6f8e9bf2206fc419633d469
Nijoane <nicajoane@outlook.com>
2020-12-05 02:53:26 -0300

    adicionando funções de filtro

--- data.js
+++ data.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 export const searchCharacter = (data, name) => {
   return data.filter(results => results.name.includes(name));
 }
@@ -6,20 +7,12 @@ export const orderAZ =(data) => data.sort((a, b) => (a.name < b.name) ? - 1 : 1)
 
 export const orderZA =(data) => data.sort((a, b) => (a.name > b.name) ? - 1 : 1);
 
+export const getSpecies = (data, specie) => {
+  return data.filter(results=> results.species.includes(specie));
 
-
-
-function percentStatus(status, resultFilter) {
-  const perc = (resultFilter.length *100)/status.length;
-  return Math.round(perc);
 }
 
-export function getSpecies(getPersonagens, species) {
-  let resultSpecies = getPersonagens.filter(el => el.species.includes(species));
-  return resultSpecies;
-};
-
-function percentSpecies(species, resultSpecies) {
-  let percent = (resultSpecies.length *100)/species.length;
-  return Math.round(percent);
+export const getStatus = (data, status) => {
+  console.log(data)
+  return data.filter(results => results.status.includes(status));
 }
